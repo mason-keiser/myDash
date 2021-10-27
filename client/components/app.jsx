db6e36ef35eb9c09e5d71b33ff919bcec04ffc7d
Mason Keiser <masonksr5@gmail.com>
2021-10-27 04:30:53 +0000

    navbar created

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dash from './dash'
 
 const App = () => {
     
     return (
-        <div>
-            app created
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Dash}/>
+            </Switch>
+        </Router>  
     )
 }
 
---
+++ client/components/nav.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+const Nav = (props) => {
+    const [input, setInput] = useState(false);
+
+    const openSearch = () => {
+        const inpt = document.querySelector(".searchInput")
+       
+        if (!input) {
+            inpt.style.display = "unset"
+            setInput(true)
+        } else {
+            inpt.style.display = "none"
+            setInput(false)
+        }
+    }
+
+    return (
+        <div>
+            <div className="navCont">
+                <div className="logoCont">
+                    <img className="logo" src="./images/mydashlogo.png" alt="" />
+                </div>
+                <div className="navIconCont">
+                    <input className="searchInput" placeholder="enter search here" type="text" />
+                    <i onClick={() => openSearch()} className="fas fa-search searchIcon"></i>
+                    <i className="fas fa-bell bellIcon"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav
